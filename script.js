132248228b650cd62086b99188bb710990dbe076
tarikjamil <tarikjamill@gmail.com>
2024-03-25 12:18:22 +0000

    edits

--- script.js
+++ script.js
@@ -58,28 +58,38 @@ document.querySelectorAll("[animation=fade]").forEach(function (fadeSplitElem) {
 
 // ------------------ navbar - accordion ------------------ //
 $(".navbar--dropdown-trigger").on("click", function () {
-  // Close other accordions when opening new one
+  // Close other accordions when opening a new one
   if (!$(this).hasClass("open")) {
     $(".navbar--dropdown-trigger.open").click();
   }
+
+  // Toggle the is--active class on the parent .navbar--dropdown
+  // It's important to toggle this before we check for the "open" class since we're about to toggle that
+  $(this).closest(".navbar--dropdown").toggleClass("is--active");
+
   // Save the sibling of the toggle we clicked on
   let sibling = $(this).siblings(".navbar--dropdown--list");
   let animationDuration = 500;
 
   if ($(this).hasClass("open")) {
     // Close the content div if already open
-    sibling.animate({ height: "0px" }, animationDuration);
+    sibling.animate({ height: "0px" }, animationDuration, function () {
+      // It might be necessary to remove is--active class here if you want to remove it only after animation
+      // However, if the is--active class should mirror the open state regardless of animation, leave the toggleClass where it is
+    });
   } else {
     // Open the content div if already closed
     sibling.css("height", "auto");
-    let autoHeight = sibling.height();
-    sibling.css("height", "0px");
-    sibling.animate({ height: autoHeight }, animationDuration, () => {
-      sibling.css("height", "auto");
+    let autoHeight = sibling.height(); // Get the full height
+    sibling.css("height", "0px"); // Reset the height
+    sibling.animate({ height: autoHeight }, animationDuration, function () {
+      sibling.css("height", "auto"); // Set the height to auto after the animation
 
       // Scroll the page to the accordion, leaving 200 pixels from the top
+      // Additional functionality for scrolling can be added here if needed
     });
   }
+
   // Open and close the toggle div
   $(this).toggleClass("open");
 });
