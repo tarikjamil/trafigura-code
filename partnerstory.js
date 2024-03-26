08f42684ff19498317b06a30a46dea46aaf81f8d
tarikjamil <tarikjamill@gmail.com>
2024-03-26 12:53:07 +0000

    Update partnerstory.js

--- partnerstory.js
+++ partnerstory.js
@@ -34,7 +34,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Create radio buttons for state filter
     states.forEach((state) => {
-      const radioHtml = `<label><input type="radio" name="state" class="radio-btn" value="${state}"> ${state}</label>`;
+      const radioHtml = `<label class="radio-btn"><input type="radio" name="state" class="radio-check" value="${state}"> ${state}</label>`;
       stateFilter.insertAdjacentHTML("beforeend", radioHtml);
     });
   }
