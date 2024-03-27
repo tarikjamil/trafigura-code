d82a6dcf7a056d95e6e0a233b4a056247884fb9b
tarikjamil <tarikjamill@gmail.com>
2024-03-27 18:04:06 +0000

    Update script.js

--- script.js
+++ script.js
@@ -104,10 +104,6 @@ const swiper = new Swiper(".is--slider", {
   loop: false,
   centeredSlides: false,
   // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
   // Define breakpoints
   breakpoints: {
     // When window width is >= 992px
@@ -115,16 +111,6 @@ const swiper = new Swiper(".is--slider", {
       slidesPerView: 1,
       spaceBetween: "20rem",
     },
-  },
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  // And if we need scrollbar
-  scrollbar: {
-    el: ".swiper-scrollbar",
-    draggable: true,
   },
 });
 
@@ -135,11 +121,6 @@ const swiper2 = new Swiper(".is--circles-slider", {
   spaceBetween: "20rem",
   loop: false,
   centeredSlides: false,
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
   // Define breakpoints
   breakpoints: {
     // When window width is >= 992px
@@ -149,13 +130,4 @@ const swiper2 = new Swiper(".is--circles-slider", {
     },
   },
   // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  // And if we need scrollbar
-  scrollbar: {
-    el: ".swiper-scrollbar",
-    draggable: true,
-  },
 });
