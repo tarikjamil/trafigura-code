17d6a86c72610e45c42cad98a0598c0682084794
tarikjamil <tarikjamill@gmail.com>
2024-03-28 16:33:44 +0000

    Update script.js

--- script.js
+++ script.js
@@ -57,41 +57,41 @@ document.querySelectorAll("[animation=fade]").forEach(function (fadeSplitElem) {
 });
 
 // ------------------ navbar - accordion ------------------ //
-$(".navbar--dropdown-trigger").on("click", function () {
-  // Close other accordions when opening a new one
-  if (!$(this).hasClass("open")) {
-    $(".navbar--dropdown-trigger.open").click();
-  }
+$(document).ready(function () {
+  // Your existing click event code for .navbar--dropdown-trigger
+  $(".navbar--dropdown-trigger").on("click", function () {
+    // Close other accordions when opening a new one
+    if (!$(this).hasClass("open")) {
+      $(".navbar--dropdown-trigger.open").click();
+    }
 
-  // Toggle the is--active class on the parent .navbar--dropdown
-  // It's important to toggle this before we check for the "open" class since we're about to toggle that
-  $(this).closest(".navbar--dropdown").toggleClass("is--active");
+    // Toggle the is--active class on the parent .navbar--dropdown
+    $(this).closest(".navbar--dropdown").toggleClass("is--active");
 
-  // Save the sibling of the toggle we clicked on
-  let sibling = $(this).siblings(".navbar--dropdown--list");
-  let animationDuration = 500;
+    let sibling = $(this).siblings(".navbar--dropdown-list");
+    let animationDuration = 500;
 
-  if ($(this).hasClass("open")) {
-    // Close the content div if already open
-    sibling.animate({ height: "0px" }, animationDuration, function () {
-      // It might be necessary to remove is--active class here if you want to remove it only after animation
-      // However, if the is--active class should mirror the open state regardless of animation, leave the toggleClass where it is
-    });
-  } else {
-    // Open the content div if already closed
-    sibling.css("height", "auto");
-    let autoHeight = sibling.height(); // Get the full height
-    sibling.css("height", "0px"); // Reset the height
-    sibling.animate({ height: autoHeight }, animationDuration, function () {
-      sibling.css("height", "auto"); // Set the height to auto after the animation
+    if ($(this).hasClass("open")) {
+      sibling.animate({ height: "0px" }, animationDuration);
+    } else {
+      sibling.css("height", "auto");
+      let autoHeight = sibling.height();
+      sibling.css("height", "0px");
+      sibling.animate({ height: autoHeight }, animationDuration, function () {
+        sibling.css("height", "auto");
+      });
+    }
 
-      // Scroll the page to the accordion, leaving 200 pixels from the top
-      // Additional functionality for scrolling can be added here if needed
-    });
-  }
+    $(this).toggleClass("open");
+  });
 
-  // Open and close the toggle div
-  $(this).toggleClass("open");
+  // Check if any .navlink has the class .w--current and trigger click on its .navbar--dropdown-trigger
+  $(".navlink.w--current").each(function () {
+    $(this)
+      .closest(".navbar--dropdown")
+      .find(".navbar--dropdown-trigger")
+      .click();
+  });
 });
 
 // ------------------ Swiper ------------------ //
