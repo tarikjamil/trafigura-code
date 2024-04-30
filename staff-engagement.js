d8590af8b8c9a2f9293e2e951b371030e6173db6
tarikjamil <tarikjamill@gmail.com>
2024-04-30 12:52:52 +0100

    Update staff-engagement.js

--- staff-engagement.js
+++ staff-engagement.js
@@ -1,18 +1,21 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var observer = new Intersection Observer(entries => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.play();
-      } else {
-        entry.target.pause();
-      }
-    });
-  }, {
-    threshold: 0.5 // Triggers when 50% of the video is visible
-  });
+document.addEventListener("DOMContentLoaded", function () {
+  var observer = new IntersectionObserver(
+    function (entries) {
+      entries.forEach(function (entry) {
+        if (entry.isIntersecting) {
+          entry.target.play();
+        } else {
+          entry.target.pause();
+        }
+      });
+    },
+    {
+      threshold: 0.5, // Triggers when 50% of the video is visible
+    }
+  );
 
-  var videos = document.querySelectorAll('video');
-  videos.forEach(video => {
+  var videos = document.querySelectorAll("video");
+  videos.forEach(function (video) {
     observer.observe(video);
   });
 });
